9f92b7a94c0891598b08f135e8ccfd4baec9c7a9
Małgorzata Mermon <malgorzata.mermon@boldare.com>
2019-10-21 15:17:46 +0200

    imp(select area zoom): quick fix - added implementation for getCanvasPoisionForPoint and getPointForCanvasPosition

--- packages/charts/src/views/chart-zoom.view.ts
+++ packages/charts/src/views/chart-zoom.view.ts
@@ -65,11 +65,23 @@ export class ChartZoomView extends ChartView<ChartZoomViewProps> {
     }
   }
 
-  getCanvasPositionForPoint(_point: XYPoint<number>): CanvasPosition {
-    throw new Error('Method not implemented.');
+  getCanvasPositionForPoint(point: XYPoint<number>): CanvasPosition {
+    const { xScale, yScale } = this.getScales();
+    const x = xScale(point.x);
+    const y = yScale(point.y);
+    return {
+      x,
+      y,
+      absoluteX: this.offsetRect.l + x,
+      absoluteY: this.offsetRect.t + y,
+    };
   }
-  getPointForCanvasPosition(_position: XYPoint<number>): XYPoint<number> | undefined {
-    throw new Error('Method not implemented.');
+  getPointForCanvasPosition(position: XYPoint<number>): XYPoint<number> | undefined {
+    const { xScale, yScale } = this.getScales();
+    return {
+      x: xScale.invert(position.x),
+      y: yScale.invert(position.y),
+    };
   }
 
 }
